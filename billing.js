2561b32f736b2cb1427fa026f6ed40244ceafbf3
Denis Kisina <kisinad@gmail.com>
2019-06-12 21:12:56 +0300

    added billing JS file

---
+++ billing.js
@@ -0,0 +1,5 @@
+//This is the percentage that determines the transaction charge
+const transactionCharge = 0.025;
+var amountTendered = 100000;
+console.log("This is what the billed amount is (less transaction charges");
+console.log(billedAmount=amountTendered-(amountTendered*transactionCharge));
