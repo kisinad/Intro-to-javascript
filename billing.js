cc005858d5f44ef2ad6ed4261ddd715251697a1b
Denis Kisina <kisinad@gmail.com>
2019-06-13 08:03:51 +0300

    Modified return

--- billing.js
+++ billing.js
@@ -2,4 +2,4 @@
 const transactionCharge = 0.025;
 var amountTendered = 100000;
 console.log("This is what the billed amount is (less transaction charges");
-console.log(billedAmount=amountTendered-(amountTendered*transactionCharge));
+console.log(amountTendered-(amountTendered*transactionCharge));
