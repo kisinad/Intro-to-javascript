b8c49a8b1d3bd80867a5125d948d24d06d15cbaa
Denis Kisina <kisinad@gmail.com>
2019-06-14 07:31:34 +0300

    added comments

--- null_checker.js
+++ null_checker.js
@@ -1,6 +1,7 @@
-for(count=1;count<=10;count++)
+for(count=1;count<=10;count++) //for loop to iterate screen prompt
 {
-	let num = prompt ("Please enter number:");
+	let num = prompt ("Please enter number:"); //Outputs prompt window
+	//Determines weather number is less than 0 or greater & outputs appropriate response
 	if (num>0)
 		console.log(1);
 	else if (num<0)
